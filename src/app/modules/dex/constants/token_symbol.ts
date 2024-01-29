5748f3ee2049689c47387411056f090af55c4dff
swapsatoshi <swapsatoshi@gmail.com>
2024-01-29 19:00:34 +0800

    change native token ID to 555555; and generate genesis block

--- src/app/modules/dex/constants/token_symbol.ts
+++ src/app/modules/dex/constants/token_symbol.ts
@@ -3,4 +3,4 @@ export const SWT_TOKEN_SYMBOL = 'SWT';
 export const LSK_TOKEN_DECIMAL = 8;
 export const SWT_TOKEN_DECIMAL = 8;
 export const LSK_TOKEN_ID = Buffer.from('00000000000000', 'hex'); // first byte not included
-export const SWT_TOKEN_ID = Buffer.from('53575400000000', 'hex'); // first byte not included
+export const SWT_TOKEN_ID = Buffer.from('55555500000000', 'hex'); // first byte not included
