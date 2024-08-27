75a98562f025f5c48fe4f4ceeb1b2cdb14142450
swapsatoshi <swapsatoshi@gmail.com>
2024-08-27 16:41:49 +0800

    fix boostMultiplier formula

--- src/app/modules/governance/utils/boost.ts
+++ src/app/modules/governance/utils/boost.ts
@@ -1,3 +1,3 @@
 export function getBoostMultiplier(currentHeight: number, targetHeight: number, maxBoostDuration: number, boostFactor: number) {
-	return 1 + Math.max(0, (targetHeight - currentHeight) / maxBoostDuration) * (boostFactor / 100 - 1);
+	return 1 + Math.max(0, (targetHeight - currentHeight) / maxBoostDuration) * Math.max(0, boostFactor / 100 - 1);
 }
